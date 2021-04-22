ffe2d3276d0868150aab390780d2cf52f9796ba4
Guzman Lopez <guzman@tryolabs.com>
2021-04-22 01:32:07 -0300

    Fix tracks url

--- src/preparation/constants.py
+++ src/preparation/constants.py
@@ -6,9 +6,7 @@ URL_HORARIOS_OMNIBUS_POR_PARADAS = (
     "http://www.montevideo.gub.uy/sites/default/files/datos/uptu_pasada_variante.zip"
 )
 URL_STM_PARADAS_OMNIBUS = "https://intgis.montevideo.gub.uy/sit/tmp/v_uptu_paradas.zip"
-URL_STM_RECORRIDOS_OMNIBUS = (
-    "https://intgis.montevideo.gub.uy/sit/tmp/uptu_variante_no_maximal.zip"
-)
+URL_STM_RECORRIDOS_OMNIBUS = "https://intgis.montevideo.gub.uy/sit/tmp/v_uptu_lsv.zip?"
 
 # Files
 FILE_STM_VIAJES_PREFIX = "stm_viajes_"
